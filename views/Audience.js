7371a56fc904fe57be4e7ab988e4c55a72bb328b
Alex Banks <alex@moonhighway.com>
2015-04-09 12:22:39 -0700

    Changing the router

---
+++ views/Audience.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Audience = React.createClass({
+
+    render() {
+        return <h1>[Audience]</h1>
+    }
+
+});
+
+module.exports = Audience;
---
+++ views/Scoreboard.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Scoreboard = React.createClass({
+    render() {
+        return <h1>[Scoreboard]</h1>
+    }
+});
+
+module.exports = Scoreboard;
